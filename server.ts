0bd54eb6cb7592a430dfbf54bacbcfb4655810d2
bangarangler <bangarangler@gmail.com>
2020-12-15 17:10:51 -0500

    pushing

--- server.ts
+++ server.ts
@@ -92,85 +92,22 @@ const main = async () => {
           "request.credentials": "include",
         },
       },
-      // context: async ({ req, res, connection, redis }: ServerContext) => {
       context: async ({ req, res, connection, redis }: ServerContext) => {
-        // const userId = req?.session?.userId || "";
-        // console.log("userId", userId);
-        // const accessToken = req.get("Authorization") || "";
-        // console.log("accessToken", accessToken);
-        // const userId = getUser(accessToken);
-        // Is it a WS connection?
-        // if (connection) {
-        //   console.log("connection from context", connection);
-        //   // console.log("connection.context", connection.context);
-        //   //   console.log("connection", connection);
-        //   // Are you logged in if you are your id is in context?
-        //   // if (!connection.context.id) {
-        //   //   throw new ApolloError("Not Authenticated");
-        //   // }
-        //   // your good get it out of context
-        //   connection.pubsub = pubsub;
-        //   return { connection };
-        // }
-        // otherwise not a WS so just pass pubsub to context
-        // return { req, res, db, pubsub };
-        return { req, res, db, redis, pubsub, connection };
+        if (connection) {
+          connection.pubsub = pubsub;
+          return { connection };
+        }
+        return { req, res, db, redis, pubsub };
       },
       subscriptions: {
-        // onConnect: (_, ws: any) => {
         onConnect: (cParams: any, __) => {
-          // console.log("cParams", cParams.accessToken);
           const accessToken = cParams.accessToken;
-          console.log("accessToken", accessToken);
           const user = validateAccessToken(accessToken);
           if (!user) {
             throw new Error("Not Authed");
           }
-          console.log("userId from onConnect", user.userId);
-          return user.userId;
-          // const accessToken = cParams.cParams;
-          // console.log("accessToken", accessToken);
-          // console.log("ws", ws.upgradedReq);
-          // return new Promise((res: any) => {
-          // console.log("res.upgradedReq", res.upgradedReq);
-          // console.log("res", res.upgradedReq);
-          // res({ req: ws.upgradedReq });
-          // });
+          return { userId: user.userId };
         },
-        // onConnect: (_, ws: any, __) => {
-        // onConnect: (connectionParams: any) => {
-        //   console.log("connectionParams", connectionParams);
-        //   if (connectionParams.authToken) {
-        //     console.log(
-        //       "connectionParams.authToken",
-        //       connectionParams.authToken
-        //     );
-        //   }
-        //     const userId = ws.upgradedReq;
-        //     console.log("userId from subscriptions", userId);
-        // return userId
-        // get cookies
-        // const tokenStr = ws.upgradeReq.headers.cookie;
-        // console.log("tokenStr", tokenStr);
-        // are there cookies in req?
-        // if (tokenStr) {
-        // seperate access= and refresh= from actual cookie value
-        // const accessToken = tokenStr.split(";")[0].split("=")[1];
-        // const refreshToken = tokenStr.split(";")[1].split("=")[1];
-        // verify the user
-        // const decodedAccessToken = validateAccessToken(accessToken);
-        // const decodedRefreshToken = validateRefreshToken(refreshToken);
-        // cookie valid?
-        // if (!decodedAccessToken || !decodedAccessToken.user) {
-        //       throw new ApolloError("Not Authenticated");
-        //     } else if (!decodedRefreshToken || !decodedRefreshToken.user) {
-        //       throw new ApolloError("Not Authenticated");
-        //     } else {
-        //       // return id in context so we can check for it below
-        //       return decodedAccessToken.user;
-        //     }
-        //   }
-        // },
       },
     });
 
@@ -192,11 +129,6 @@ const main = async () => {
     } catch (err) {
       console.log("err from httpServerr connection", err);
     }
-    // app.listen({ port: process.env.PORT }, () => {
-    //   console.log(
-    //     `Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
-    //   );
-    // });
   } catch (err) {
     console.log("hold up main is busted");
   }
